73187e56d4514cbcc9603db6f788c7accdca200d
Edward Ascan <edward.ascan@Edwards-Air.lan>
2021-04-12 13:07:28 -0400

    fixing navbar

---
+++ assets/js/effects.js
@@ -0,0 +1,20 @@
+// $(document).on("scroll", function () {
+//   if ($(window).scrollTop() > 30) {
+//     $(".navbar").css({
+//       "background-color": "yellow !important",
+//       "box-shadow": "-1px 9px 40px -12px #2e3047 !important",
+//     });
+//   } else {
+//     $(".navbar").css({
+//       "background-color": "#2e3047 !important",
+//       "box-shadow": "none",
+//     });
+//   }
+// });
+
+$(function () {
+  $(document).scroll(function () {
+    var $nav = $(".sticky-top");
+    $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
+  });
+});
